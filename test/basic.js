7d8f4fc2743743203242364d69674a25ce7639ab
isaacs <i@izs.me>
2022-02-26 21:49:22 -0800

    ci and make tests pass with new tap

--- test/basic.js
+++ test/basic.js
@@ -3,18 +3,18 @@ var Yallist = require('../yallist.js')
 
 var y = new Yallist(1, 2, 3, 4, 5)
 var z = new Yallist([1, 2, 3, 4, 5])
-t.similar(y, z, 'build from single list or args')
+t.match(y, z, 'build from single list or args')
 
 function add10 (i) {
   return i + 10
 }
-t.similar(y.map(add10).toArray(), [11, 12, 13, 14, 15])
-t.similar(y.mapReverse(add10).toArray(), [15, 14, 13, 12, 11])
+t.match(y.map(add10).toArray(), [11, 12, 13, 14, 15])
+t.match(y.mapReverse(add10).toArray(), [15, 14, 13, 12, 11])
 
-t.similar(y.map(add10).toArrayReverse(), [15, 14, 13, 12, 11])
-t.isa(Yallist(1, 2, 3), 'Yallist')
+t.match(y.map(add10).toArrayReverse(), [15, 14, 13, 12, 11])
+t.type(Yallist(1, 2, 3), 'Yallist')
 t.equal(y.push(6, 7, 8), 8)
-t.similar(y.toArray(), [1, 2, 3, 4, 5, 6, 7, 8])
+t.match(y.toArray(), [1, 2, 3, 4, 5, 6, 7, 8])
 y.pop()
 y.shift()
 y.unshift(100)
@@ -22,7 +22,7 @@ y.unshift(100)
 var expect = [100, 2, 3, 4, 5, 6, 7]
 var expectReverse = [ 7, 6, 5, 4, 3, 2, 100 ]
 
-t.similar(y.toArray(), expect)
+t.match(y.toArray(), expect)
 t.equal(y.length, y.toArray().length)
 
 t.test(function forEach (t) {
@@ -62,12 +62,12 @@ t.equal(Yallist().shift(), undefined)
 var x = Yallist()
 x.unshift(1)
 t.equal(x.length, 1)
-t.similar(x.toArray(), [1])
+t.match(x.toArray(), [1])
 
 // verify that y.toArray() returns an array and if we create a
 // new Yallist from that array, we get a list matching it
-t.similar(Yallist(y.toArray()), y)
-t.similar(Yallist.apply(null, y.toArray()), y)
+t.match(Yallist(y.toArray()), y)
+t.match(Yallist.apply(null, y.toArray()), y)
 
 t.throws(function () {
   new Yallist().reduce(function () {})
@@ -78,9 +78,9 @@ t.throws(function () {
 
 z = y.reverse()
 t.equal(z, y)
-t.similar(y.toArray(), expectReverse)
+t.match(y.toArray(), expectReverse)
 y.reverse()
-t.similar(y.toArray(), expect)
+t.match(y.toArray(), expect)
 
 var a = Yallist(1, 2, 3, 4, 5, 6)
 var cases = [
@@ -98,8 +98,8 @@ t.test('slice', function (t) {
   t.plan(cases.length)
   cases.forEach(function (c) {
     t.test(JSON.stringify(c), function (t) {
-      t.similar(a.slice.apply(a, c[0]), Yallist(c[1]))
-      t.similar([].slice.apply(a.toArray(), c[0]), c[1])
+      t.match(a.slice.apply(a, c[0]), Yallist(c[1]))
+      t.match([].slice.apply(a.toArray(), c[0]), c[1])
       t.end()
     })
   })
@@ -110,8 +110,8 @@ t.test('sliceReverse', function (t) {
   cases.forEach(function (c) {
     var rev = c[1].slice().reverse()
     t.test(JSON.stringify([c[0], rev]), function (t) {
-      t.similar(a.sliceReverse.apply(a, c[0]), Yallist(rev))
-      t.similar([].slice.apply(a.toArray(), c[0]).reverse(), rev)
+      t.match(a.sliceReverse.apply(a, c[0]), Yallist(rev))
+      t.match([].slice.apply(a.toArray(), c[0]).reverse(), rev)
       t.end()
     })
   })
@@ -119,42 +119,42 @@ t.test('sliceReverse', function (t) {
 
 var inserter = Yallist(1, 2, 3, 4, 5)
 inserter.unshiftNode(inserter.head.next)
-t.similar(inserter.toArray(), [2, 1, 3, 4, 5])
+t.match(inserter.toArray(), [2, 1, 3, 4, 5])
 inserter.unshiftNode(inserter.tail)
-t.similar(inserter.toArray(), [5, 2, 1, 3, 4])
+t.match(inserter.toArray(), [5, 2, 1, 3, 4])
 inserter.unshiftNode(inserter.head)
-t.similar(inserter.toArray(), [5, 2, 1, 3, 4])
+t.match(inserter.toArray(), [5, 2, 1, 3, 4])
 
 var single = Yallist(1)
 single.unshiftNode(single.head)
-t.similar(single.toArray(), [1])
+t.match(single.toArray(), [1])
 
 inserter = Yallist(1, 2, 3, 4, 5)
 inserter.pushNode(inserter.tail.prev)
-t.similar(inserter.toArray(), [1, 2, 3, 5, 4])
+t.match(inserter.toArray(), [1, 2, 3, 5, 4])
 inserter.pushNode(inserter.head)
-t.similar(inserter.toArray(), [2, 3, 5, 4, 1])
+t.match(inserter.toArray(), [2, 3, 5, 4, 1])
 inserter.unshiftNode(inserter.head)
-t.similar(inserter.toArray(), [2, 3, 5, 4, 1])
+t.match(inserter.toArray(), [2, 3, 5, 4, 1])
 
 single = Yallist(1)
 single.pushNode(single.tail)
-t.similar(single.toArray(), [1])
+t.match(single.toArray(), [1])
 
 var swiped = Yallist(9, 8, 7)
 inserter.unshiftNode(swiped.head.next)
-t.similar(inserter.toArray(), [8, 2, 3, 5, 4, 1])
-t.similar(swiped.toArray(), [9, 7])
+t.match(inserter.toArray(), [8, 2, 3, 5, 4, 1])
+t.match(swiped.toArray(), [9, 7])
 
 swiped = Yallist(9, 8, 7)
 inserter.pushNode(swiped.head.next)
-t.similar(inserter.toArray(), [8, 2, 3, 5, 4, 1, 8])
-t.similar(swiped.toArray(), [9, 7])
+t.match(inserter.toArray(), [8, 2, 3, 5, 4, 1, 8])
+t.match(swiped.toArray(), [9, 7])
 
 swiped.unshiftNode(Yallist.Node(99))
-t.similar(swiped.toArray(), [99, 9, 7])
+t.match(swiped.toArray(), [99, 9, 7])
 swiped.pushNode(Yallist.Node(66))
-t.similar(swiped.toArray(), [99, 9, 7, 66])
+t.match(swiped.toArray(), [99, 9, 7, 66])
 
 var e = Yallist()
 e.unshiftNode(Yallist.Node(1))
